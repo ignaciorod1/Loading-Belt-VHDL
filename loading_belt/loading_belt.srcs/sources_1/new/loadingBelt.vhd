1db7e58c302cee099c9092a6917ad10b5bd03dab
ignaciorod1 <ignacio.rodriguez.naranjo@alumnos.upm.es>
2018-12-12 10:15:16 +0100

    first commit

---
+++ loading_belt/loading_belt.srcs/sources_1/new/loadingBelt.vhd
@@ -0,0 +1,23 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity loadingBelt is
+--  Port ( );
+end loadingBelt;
+
+architecture Behavioral of loadingBelt is
+
+begin
+
+
+end Behavioral;
