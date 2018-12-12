4cbe12f6cdb8a0a7446b7266fd8ff0a195bf57ba
CarlosPortero <porterofernandezcarlos@gmail.com>
2018-12-12 10:45:19 +0100

    señales

--- loading_belt/loading_belt.srcs/sources_1/new/loadingBelt.vhd
+++ loading_belt/loading_belt.srcs/sources_1/new/loadingBelt.vhd
@@ -12,12 +12,23 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity loadingBelt is
---  Port ( );
+ Port (
+    CLK: IN STD_LOGIC;
+    RESET: IN STD_LOGIC;
+    START: IN STD_LOGIC;
+    SW0: IN STD_LOGIC;
+    P1: IN STD_LOGIC;
+    P2: IN STD_LOGIC;
+    ENSTOP: IN STD_LOGIC;
+    BITROBOT: OUT STD_LOGIC
+  );
 end loadingBelt;
 
 architecture Behavioral of loadingBelt is
-
+TYPE estados IS (S0, S1, S2, S3);
+SIGNAL state0, state1, state2, state3: estados;
+--deberiamos declarar BITROBOT como SEÑAL?
 begin
-
+ 
 
 end Behavioral;
