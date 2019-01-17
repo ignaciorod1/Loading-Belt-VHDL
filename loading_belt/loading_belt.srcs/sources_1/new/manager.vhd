d22916dabd7d1c20a05c0fc098d4d3273dd955ea
ignaciorod1 <ignacio.rodriguez.naranjo@alumnos.upm.es>
2019-01-17 15:42:44 +0100

    clock divider

---
+++ loading_belt/loading_belt.srcs/sources_1/new/manager.vhd
@@ -0,0 +1,21 @@
+-- Manager entity to handle all the programs
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity manager is
+  PORT (
+
+  	clk: IN STD_LOGIC;
+  	reset: IN STD_LOGIC;
+  	servo: IN STD_LOGIC
+  	  	);
+end manager;
+
+architecture Behavioral of manager is
+
+begin
+
+
+end Behavioral;
